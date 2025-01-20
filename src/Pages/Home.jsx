7e13224cedede680fbdadb8cd23c357d021f9642
nischay gadher <nischaygadher15@gmail.com>
2025-01-20 11:59:42 +0530

    Home page basic structure done.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,15 @@ import React, { useEffect, useId, useRef, useState } from "react";
 import banner1 from "../assets/images/PTSWEBSITELAUNCH.jpeg";
 import banner2 from "../assets/images/biologo (1)-min.jpg";
 import { NavLink } from "react-router-dom";
-import { Button, Card, Carousel, Progress } from "flowbite-react";
+import {
+  Button,
+  Card,
+  Carousel,
+  Label,
+  Progress,
+  Textarea,
+  TextInput,
+} from "flowbite-react";
 import project from "../assets/images/website2.jpg";
 import "../App.css";
 import { FaGoogle, FaInstagram, FaPlus } from "react-icons/fa";
@@ -494,7 +502,7 @@ const Home = () => {
       </section>
 
       {/* Happy Clients Works */}
-      <section className="px-10 py-24 max-w-screen-sm min-h-screen md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
+      <section className="px-10 py-24 w-screen min-h-screen md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
         <div className="w-1/2 text-black font-semibold text-lg mb-10">
           <span className="text-5xl font-semibold">Happy Clients Works</span>
           <div className="flex items-center mt-3">
@@ -541,8 +549,66 @@ const Home = () => {
       </section>
 
       {/* Contact Us Form */}
-      <section className="px-10 min-h-screen py-24 max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
-        <form action="#"></form>
+      <section className="px-10 w-screen min-h-screen py-24 max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
+        <div className="w-1/2 text-black font-semibold text-lg mb-10">
+          <span className="text-5xl font-semibold">Contact Us</span>
+          <div className="flex items-center mt-3">
+            <span className="bg-[#8490FF] w-4 h-4 rounded-full"></span>
+            <span className="bg-[#8490FF] h-1 w-52"></span>
+          </div>
+        </div>
+        <div className="w-full flex justify-center">
+          <form action="#" className="w-1/2">
+            <div className="mb-3 flex items-center gap-6">
+              <div className="w-1/2">
+                <TextInput
+                  id="username"
+                  type="text"
+                  placeholder="Username"
+                  helperText={
+                    <span className="font-medium">Error Message here</span>
+                  }
+                />
+              </div>
+              <div className="w-1/2">
+                <TextInput
+                  id="email"
+                  type="text"
+                  placeholder="Email"
+                  helperText={
+                    <span className="font-medium">Error Message here</span>
+                  }
+                />
+              </div>
+            </div>
+            <div className="mb-3">
+              <TextInput
+                id="subject"
+                type="text"
+                placeholder="Subject"
+                helperText={
+                  <span className="font-medium">Error Message here</span>
+                }
+              />
+            </div>
+            <div className="mb-7">
+              <Textarea
+                id="message"
+                placeholder="Leave a message here..."
+                rows={9}
+                helperText={
+                  <span className="font-medium">Error Message here</span>
+                }
+              />
+            </div>
+
+            <div className="mb-7">
+              <Button type="submit" color="blue">
+                SEND MESSAGE
+              </Button>
+            </div>
+          </form>
+        </div>
       </section>
     </>
   );
